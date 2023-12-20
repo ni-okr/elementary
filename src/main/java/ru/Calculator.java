da435197b57c9b091500bec7e3a1cc92b314ef5d
Роман Новиков <chVeZcv[jALy\n:(>
2023-12-20 23:32:51 +0300

    1.3 Арифметические операции

---
+++ src/main/java/ru/Calculator.java
@@ -0,0 +1,21 @@
+package ru;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int onePlusTwo = one + two;
+//дз
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int fourTimeTwo = four * two;
+
+        System.out.println("onePlusTwo = " + onePlusTwo);
+        System.out.println("sixDivTwo = " + sixDivTwo);
+        System.out.println("fiveMinusTwo = " + fiveMinusTwo);
+        System.out.println("fourTimeTwo = " + fourTimeTwo);
+    }
+}
