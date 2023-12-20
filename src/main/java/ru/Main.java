2cc3f947d68e4d33fea64c323c3f4cf05e9be700
Роман Новиков <chVeZcv[jALy\n:(>
2023-12-20 14:51:39 +0300

    проверка подписи к комитам

--- src/main/java/ru/Main.java
+++ src/main/java/ru/Main.java
@@ -2,6 +2,6 @@ package ru;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Поздравляю Председателя Свободной КНДР с успешным запуском!");
+        System.out.println("Поздравляю Председателя Свободной КНДР с успешным запуском в епонском море!");
     }
 }
