5bc5f27596cc8bc29bd2022b8b67f5c4e9e8b4de
Роман Новиков <chVeZcv[jALy\n:(>
2023-12-21 14:14:10 +0300

    1.7 правки

--- src/main/java/ru/calculator/MathFunc.java
+++ src/main/java/ru/calculator/MathFunc.java
@@ -2,14 +2,12 @@ package ru.calculator;
 
 public class MathFunc {
 
-    public static int func1(int args) {
-        int x = 3;
+    public static int func1(int x) {
         int y = x * x + 1;
         return y;
     }
 
-    public static int func2(int args) {
-        int x = 3;
+    public static int func2(int x) {
         int y = 1 / x;
         return y;
     }
@@ -20,7 +18,7 @@ public class MathFunc {
         int total = result1 + result2;
         System.out.println("total = " + total);
 
-        int result3 = MathFunc.func1(3);
+        int result3 = MathFunc.func1(100);
         System.out.println("result3 = " + result3);
     }
 }
