29103da4f43df0ef6cdb5d3a2eb479f348143779
Роман Новиков <chVeZcv[jALy\n:(>
2023-12-21 09:57:52 +0300

    1.6 Аргументы (исправления)

--- src/main/java/ru/Calculator.java
+++ src/main/java/ru/Calculator.java
@@ -1,13 +1,27 @@
 package ru;
 
-class ArgMethod {
-    public static void hello(String name, int age) {
-        System.out.println("Hello, " + name + ", age = " + age);
-    }
-
+public class Calculator {
     public static void main(String[] args) {
-        String name = "Java";
-        int age = 6;
-        ArgMethod.hello(name, age);
+        int one = 1;
+        int two = 2;
+        int onePlusTwo = one + two;
+//дз 1.3
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int fourTimeTwo = four * two;
+
+//дз 1.4
+        int size = 10;
+        size = 100;
+        size = size - 5;
+        System.out.println("sizeMinusFive = " + size);
+
+        System.out.println("onePlusTwo = " + onePlusTwo);
+        System.out.println("sixDivTwo = " + sixDivTwo);
+        System.out.println("fiveMinusTwo = " + fiveMinusTwo);
+        System.out.println("fourTimeTwo = " + fourTimeTwo);
     }
 }
