5989ba8db1792bbad48f25d3f753f49c3c596023
Роман Новиков <chVeZcv[jALy\n:(>
2023-12-21 14:01:32 +0300

    правки в MathFunc

--- src/main/java/ru/calculator/MathFunc.java
+++ src/main/java/ru/calculator/MathFunc.java
@@ -8,7 +8,18 @@ public class MathFunc {
         return y;
     }
 
+    public static int func2(int args) {
+        int x = 3;
+        int y = 1 / x;
+        return y;
+    }
+
     public static void main(String[] args) {
+        int result1 = MathFunc.func1(3);
+        int result2 = MathFunc.func2(3);
+        int total = result1 + result2;
+        System.out.println("total = " + total);
+
         int result3 = MathFunc.func1(3);
         System.out.println("result3 = " + result3);
     }
