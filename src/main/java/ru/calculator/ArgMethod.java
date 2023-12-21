3a6c7a99f39dd73c56a387323c580930b4e1c534
Роман Новиков <chVeZcv[jALy\n:(>
2023-12-21 14:01:10 +0300

    1.7 Результат работы метода (исправления)

--- src/main/java/ru/calculator/ArgMethod.java
+++ src/main/java/ru/calculator/ArgMethod.java
@@ -4,6 +4,7 @@ public class ArgMethod {
     public static void hello(String name, int age) {
         System.out.println("Hello, " + name + ", age = " + age);
     }
+
     public static void main(String[] args) {
         String name = "Java";
         int age = 6;
