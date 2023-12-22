2f77f5a1dc9cfdd4433c939d3add1ac1287f228e
Роман Новиков <chVeZcv[jALy\n:(>
2023-12-22 10:37:23 +0300

    1.14 String

---
+++ src/main/java/ru/calculator/Greeting.java
@@ -0,0 +1,10 @@
+package ru.calculator;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java ";
+        System.out.println(idea + "But I am a newbie.");
+        int year = 2023;
+        System.out.println(year);
+    }
+}
