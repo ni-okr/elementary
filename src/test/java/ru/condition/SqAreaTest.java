ae020188dee38991cfe057dd2acded485c98926c
Роман Новиков <chVeZcv[jALy\n:(>
2023-12-23 18:26:19 +0300

    добавил дот названий в SqAreaTest и сменил тип переменных ОР на float там где дроби

--- src/test/java/ru/condition/SqAreaTest.java
+++ src/test/java/ru/condition/SqAreaTest.java
@@ -13,42 +13,42 @@ class SqAreaTest {
         int p = 6;
         double k = 2;
         double out = SqArea.square(p, k);
-        assertThat(out).isEqualTo(expected, withPrecision(1.0));
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
-    void whenP5K1Square1() {
-        int expected = 1;
+    void whenP5K1Square1Dot56() {
+        float expected = 1.56f;
         int p = 5;
         double k = 1;
         double out = SqArea.square(p, k);
-        assertThat(out).isEqualTo(expected, withPrecision(1.0));
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
-    void whenP16K12Square4() {
-        int expected = 4;
+    void whenP16K12Square4Dot54() {
+        float expected = 4.54f;
         int p = 16;
         double k = 12;
         double out = SqArea.square(p, k);
-        assertThat(out).isEqualTo(expected, withPrecision(1.0));
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
-    void whenP26K32Square4() {
-        int expected = 4;
+    void whenP26K32Square4Dot96() {
+        float expected = 4.96f;
         int p = 26;
         double k = 32;
         double out = SqArea.square(p, k);
-        assertThat(out).isEqualTo(expected, withPrecision(1.0));
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
-    void whenP61K21Square40() {
-        int expected = 40;
+    void whenP61K21Square40Dot36() {
+        float expected = 40.36f;
         int p = 61;
         double k = 21;
         double out = SqArea.square(p, k);
-        assertThat(out).isEqualTo(expected, withPrecision(1.0));
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
 }
