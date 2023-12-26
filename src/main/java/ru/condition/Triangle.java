e32dccff0bcb8f9d209e7da1acc031bd0f994f2d
Роман Новиков <chVeZcv[jALy\n:(>
2023-12-26 11:59:38 +0300

    3.13 убрал тернарную конструкцию

--- src/main/java/ru/condition/Triangle.java
+++ src/main/java/ru/condition/Triangle.java
@@ -2,7 +2,7 @@ package ru.condition;
 
 public class Triangle {
     public static boolean exist(double ab, double ac, double bc) {
-        return ab + ac > bc && ac + bc > ab && ab + bc > ac ? true : false;
+        return ab + ac > bc && ac + bc > ab && ab + bc > ac;
     }
 
     public static void main(String[] args) {
