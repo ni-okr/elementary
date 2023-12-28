7396fbf18618f6d2c20e1569776a6dfd1a01c345
Роман Новиков <chVeZcv[jALy\n:(>
2023-12-28 19:24:09 +0300

    4.6 Построить_шахматную_доску_в_псевдографике

---
+++ src/main/java/ru/loop/Board.java
@@ -0,0 +1,25 @@
+package ru.loop;
+
+public class Board {
+    public static void paint(int width, int height) {
+        for (int row = 0; row < width; row++) {
+            for (int column = 0; column < height; column++) {
+
+                if ((row + column) % 2 == 0) {
+                    System.out.print(" X ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        paint(3, 3);
+        System.out.println();
+        paint(4, 4);
+        System.out.println();
+        paint(8, 8);
+    }
+}
