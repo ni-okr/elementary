fe2dabd981cc7d91815d6c17c8a5a31116f45f9f
Роман Новиков <chVeZcv[jALy\n:(>
2023-12-28 21:01:00 +0300

    правки в шахматную доску

--- src/main/java/ru/loop/Board.java
+++ src/main/java/ru/loop/Board.java
@@ -2,9 +2,8 @@ package ru.loop;
 
 public class Board {
     public static void paint(int width, int height) {
-        for (int row = 0; row < width; row++) {
-            for (int column = 0; column < height; column++) {
-
+        for (int row = 0; row < height; row++) {
+            for (int column = 0; column < width; column++) {
                 if ((row + column) % 2 == 0) {
                     System.out.print(" X ");
                 } else {
