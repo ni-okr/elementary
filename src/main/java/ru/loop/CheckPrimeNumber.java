d1423cb6ba9fb2d43723e3479083fa6f7095ae79
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-02 14:26:33 +0300

    4.8 Правки

--- src/main/java/ru/loop/CheckPrimeNumber.java
+++ src/main/java/ru/loop/CheckPrimeNumber.java
@@ -2,13 +2,12 @@ package ru.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = false;
-        for (int index = 1; index <= number; index++) {
+        boolean prime = number > 1;
+        for (int index = 2; index < number; index++) {
             if (number % index == 0) {
-                prime = true;
+                prime = false;
                 break;
             }
-            break;
         }
         return prime;
     }
