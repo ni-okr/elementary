522a711a14215d6507be91ac8bb081951f21d0a4
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-03 23:47:55 +0300

    4.12 Крест в псевдографике

--- src/main/java/ru/loop/Slash.java
+++ src/main/java/ru/loop/Slash.java
@@ -1,26 +1,8 @@
 package ru.loop;
 
 public class Slash {
-//    public static void draw(int size) {
-//        for (int row = 0; row < size; row++) {
-//            for (int cell = 0; cell < size; cell++) {
-//                boolean left = (cell == row);
-//                boolean right = (cell == (row % 2));
-//                if (left) {
-//                    System.out.print("0");
-//                } else if (right) {
-//                    System.out.print("0");
-//                } else {
-//                    System.out.print(" ");
-//                }
-//            }
-//            System.out.println();
-//        }
-//        System.out.println();
-//    }
-
     public static void draw(int size) {
-        for (int row = 0; row <= size; row++) {
+        for (int row = 0; row < size; row++) {
             for (int cell = 0; cell <= size; cell++) {
                 boolean left = (cell == row);
                 boolean right = (cell == size - row - 1);
@@ -34,6 +16,7 @@ public class Slash {
             }
             System.out.println();
         }
+        System.out.println();
     }
 
     public static void main(String[] args) {
