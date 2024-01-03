733c06156be0d224862534b312ccba5a2ea261e8
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-03 16:12:35 +0300

    4.10 Ипотека

---
+++ src/main/java/ru/loop/Mortgage.java
@@ -0,0 +1,14 @@
+package ru.loop;
+
+public class Mortgage {
+    public static int year(double amount, int salary, double percent) {
+        int year = 0;
+        percent = percent / 100;
+        while (amount > 0) {
+            amount = amount + (amount * percent);
+            amount = amount - salary;
+            year++;
+        }
+        return year;
+    }
+}
