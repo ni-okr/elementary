ec9317b8e7c2e58b6baa65be05e567eaa5e0f20c
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-04 22:03:48 +0300

    5.4 Массивы и цикл for (правки)

--- src/main/java/ru/array/ArrayLoop.java
+++ src/main/java/ru/array/ArrayLoop.java
@@ -2,13 +2,12 @@ package ru.array;
 
 public class ArrayLoop {
     public static void main(String[] args) {
-        int[] m = new int[5];
-        for (int i = 0; i < m.length; i++) {
-            int y = i * 2 + 3;
-            m[i] = y;
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i * 2 + 3;
         }
-        for (int i = 0; i < m.length; i++) {
-            System.out.println(m[i]);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
         }
     }
 }
