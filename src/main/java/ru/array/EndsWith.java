3a8dc686befb5218f25d4a89193fb07ba0039fc6
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-06 22:27:56 +0300

    5.17 исправил алгоритм

--- src/main/java/ru/array/EndsWith.java
+++ src/main/java/ru/array/EndsWith.java
@@ -4,8 +4,9 @@ public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
         for (int index = 0; index < post.length; index++) {
-            if (post[index] != word[word.length - post.length + index]) {
+            if (post[post.length - index - 1] != word[word.length - index - 1]) {
                 result = false;
+                break;
             }
         }
         return result;
