a651268705b5b9f678b649b22705850b11a46f7c
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-06 18:01:58 +0300

    5.15 убрал цикл

--- src/main/java/ru/array/EqLast.java
+++ src/main/java/ru/array/EqLast.java
@@ -2,10 +2,8 @@ package ru.array;
 
 public class EqLast {
     public static boolean check(int[] left, int[] right) {
-        for (int i = 0; i < left.length; i++) {
-            if (left[left.length - 1] != right[right.length - 1]) {
-                return false;
-            }
+        if (left[left.length - 1] != right[right.length - 1]) {
+            return false;
         }
         return true;
     }
