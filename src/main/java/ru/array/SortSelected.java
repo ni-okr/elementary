cc4e0cde23bd9f59769c1631e9395301764c15a2
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-08 14:13:03 +0300

    5.21 Сортировка выборкой (переименовал индекс что б потом понятно было)

--- src/main/java/ru/array/SortSelected.java
+++ src/main/java/ru/array/SortSelected.java
@@ -4,8 +4,8 @@ public class SortSelected {
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
             int min = MinDiapason.findMin(data, i, data.length - 1);
-            int index = FindLoop.indexInRange(data, min, i, data.length - 1);
-            SwitchArray.swap(data, index, i);
+            int startIndex = FindLoop.indexInRange(data, min, i, data.length - 1);
+            SwitchArray.swap(data, startIndex, i);
         }
         return data;
     }
