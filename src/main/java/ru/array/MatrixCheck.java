bf53f5dc153a817659434528e9d8aa752a4bc03d
Роман Новиков <chVeZcv[jALy\n:(>
2024-01-10 14:32:45 +0300

    5.27 Моно строка в матрице (переделал)

--- src/main/java/ru/array/MatrixCheck.java
+++ src/main/java/ru/array/MatrixCheck.java
@@ -4,7 +4,7 @@ public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
         for (int i = 0; i < board.length; i++) {
-            if (board[row][0] != board[row][(board.length - 1) - i]) {
+            if (board[row][0] != board[row][i]) {
                 result = false;
                 break;
             }
